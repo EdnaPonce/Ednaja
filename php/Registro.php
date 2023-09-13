7db42ff9bd180912ea5611b6b3943afbc009d15e
EdnaPonce <101840042+EdnaPonce@users.noreply.github.com>
2023-09-13 11:33:32 -0600

    Update Registro.php

--- php/Registro.php
+++ php/Registro.php
@@ -1,22 +1,38 @@
 <?php
+$server = "localhost";
+$database = "maquinasalegria";
+$username = "ponce";
+$password = "12345";
 
-include "Conexion.php";
+// Conectarse a la base de datos
+$con = mysqli_connect($server, $username, $password, $database);
 
-
-
-$sql = mysqli_query($con, "INSERT INTO usuario (id,nombre,correo,password,telefono,edad,pais) 
-    values (NULL,'".$_POST['nombre']."', '".$_POST['correo']."', '".$_POST['password']."',
-    '".$_POST['telefono']."','".$_POST['Edad']."','".$_POST['Pais']."')");
-
-
-if($sql){
-    header("location:/Proyecto Final/html/Home.html");
-}
-else{
-    echo " Usuario no agregado";
+if (!$con) {
+    die("No hay conexi      n: " . mysqli_connect_error());
 }
 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Obtener los valores del formulario
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $password = $_POST['password'];
+    $telefono = $_POST['telefono'];
+    $edad = $_POST['Edad'];
+$pais = $_POST['Pais'];
 
+    // Crear la consulta SQL para insertar el usuario en la base de datos
+    $sql = "INSERT INTO usuario (nombre, correo, password, telefono, Edad, Pais>
 
+    // Ejecutar la consulta SQL
+    if (mysqli_query($con, $sql)) {
+        header('Location: ../html/Home.html');
+        exit();
+    } else {
+        echo "Error al registrar el usuario: " . mysqli_error($con);
+    }
+}
+
+// Cerrar la conexi      n a la base de datos
+mysqli_close($con);
 ?>
 
